f4c27b137e9eb5f0b40b30e9e79a22762acceb75
tamoshreemallick <tamoshreemallick.12@gmail.com>
2023-06-13 14:46:53 +0530

    Sharda Swiggy Basic Structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Welcome to Sharda Swiggy😋!\n\nApki Bhuk Mitane Hum Agagye hai!\nKripiya Food Menu Pe Prasthan Kare⬇️ ");
+    }
+}
